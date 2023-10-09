0e1a8b7c98820c08e763a114d0fd39574a35bdcc
Vyas <41269385+iamvyas@users.noreply.github.com>
2023-10-09 13:17:29 +0530

    Create readable_double.java

---
+++ readable_double.java
@@ -0,0 +1,8 @@
+public class readable_double
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		Double db = 100_11_1.20_0D; //(Java 7 feature)just exist to make readablility better, like how smart phones show digits splitted
+		System.out.println(db);
+	}
+}
