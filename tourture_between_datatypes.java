e4605f140a6093b95d5b146772f85da237126464
Vyas <41269385+iamvyas@users.noreply.github.com>
2023-10-09 13:45:31 +0530

    Update tourture_between_datatypes.java

--- tourture_between_datatypes.java
+++ tourture_between_datatypes.java
@@ -7,11 +7,11 @@ public class Main
 		int i='A';
 		System.out.println(" int of i = A is "+ i); //ascii value 65 is stored (how the hell im even going to remember this in exam)
 		double d = 2e2;
-		System.out.println(" double of d = 2e2 is "+ d); //2e2 mean 2x 10^2
+		System.out.println(" double of d = 2e2 is "+ d); //2e2 mean 2x 10^2 so 200.0 is displayed
 		char c =32;
 		System.out.println(" char of c = 32 is "+ c);  //displays a white space because ascii value of 32 is just a space lol
 		long l= 5^2;
-		System.out.println(" long of l = 5^2 is "+ l);
+		System.out.println(" long of l = 5^2 is "+ l); // displayed as 7
 		if(i<d && d>c || i>l && i>c || c>l){
 		    System.out.println("c/l "+ c/l); //this is displayed as output 4...but how and why 
 		}
