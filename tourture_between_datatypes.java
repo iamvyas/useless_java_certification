92088c6030736cf76e3f885f3c4ef06510f0113a
Vyas <41269385+iamvyas@users.noreply.github.com>
2023-10-09 14:03:58 +0530

    Update tourture_between_datatypes.java

--- tourture_between_datatypes.java
+++ tourture_between_datatypes.java
@@ -11,7 +11,8 @@ public class Main
 		char c =32;
 		System.out.println(" char of c = 32 is "+ c);  //displays a white space because ascii value of 32 is just a space lol
 		long l= 5^2;
-		System.out.println(" long of l = 5^2 is "+ l); // displayed as 7
+		System.out.println(" long of l = 5^2 is "+ l); // displayed as 7, because ^ is not used as exponential operator but is used as an XOR operator
+		//which basically does an XOR operation between binary values of 5(0101) and 2(0010) which is 7(0111) , Seems Java certifications are Nolanesque for no reason
 		if(i<d && d>c || i>l && i>c || c>l){
 		    System.out.println("c/l "+ c/l); //this is displayed as output 4 because it comes down to 32/7 , question is how l is 7
 		}
